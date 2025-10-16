23228b66c22f93ef9a981e9dceb385f8c796649d
smg2231 <flower91634@outlook.com>
2025-10-16 15:22:10 -0400

    Proper Character Fix

--- asset/js/todos.js
+++ asset/js/todos.js
@@ -1,13 +1,11 @@
 $(".todoinput").keypress(function(event) {
-	textLimit = 50;
-	if($(this).val().length >= textLimit) {
-		alert("You have reached the text limit of "+textLimit+" characters.");
 	if(event.which === 13) {
 		if($(this).val()==="") return;
+		if($(this).val().length >= 50) return;
 		$("ul").prepend("<li><span class='left'><i class='fa fa-trash'></i></span>  "+$(this).val()+"<span class='right'><i class='fa fa-pencil'></li>");
 		$(this).val("");
+
 	}
-}
 });
 
 $(".fa-plus").on("click", function() {
